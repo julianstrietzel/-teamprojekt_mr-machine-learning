fc2bf6c998f814132ec03d4de3d2c0e32d9484a0
isi-e <quietschquatsch@gmx.de>
2022-06-30 15:50:47 +0200

    Display first icon is working.

--- TSE-EDSwMR/Assets/POV_1.cs
+++ TSE-EDSwMR/Assets/POV_1.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class POV_1 : MonoBehaviour
 {
-    public GameObject icon1;
-    public GameObject test;
+    public GameObject icon1_prefab;
+    private GameObject icon1;
     // Start is called before the first frame update
     void Start()
     {
-        icon1 = test.getComponent<HotIcon>;
+        //  icon1 = test.getComponent<HotIcon>;
+
+        //icon1.transform.position = new Vector3(0.04f, 0.04f, 0.025f);
+         DisplayIcons();
+
     }
 
     // Update is called once per frame
@@ -20,6 +24,9 @@ public class POV_1 : MonoBehaviour
 
     public void DisplayIcons()
     {
+        // icon1 = GameObject.Instantiate(icon1_prefab, gameObject.transform);
+        icon1 = GameObject.Instantiate(icon1_prefab, gameObject.transform);
+        icon1.transform.localPosition = new Vector3(-0.04f, 0.04f, -0.025f);
 
     }
 }
